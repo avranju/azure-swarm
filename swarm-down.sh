c55d75820240348c68e9618e6643c5164a944f1e
Rajasekharan Vengalil <avranju@gmail.com>
2015-08-31 00:13:15 -0700

    Made setup and tear down scripts a tad more generic and configurable. The scripts can now be run multiple times safely and the tear down script can also be run multiple times if needed.

--- swarm-down.sh
+++ swarm-down.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+# check that deployment file name is passed in
+if [ -z "$1" ];
+then
+	echo "Please pass in the deployment file name"
+	exit 1
+fi
+
+# deployment id is passed as a command line param
+NAME_SUFFIX=`cat $1`
+
 # script parameters
-VNET_NAME=swarmvnet
-VNET_LOCATION="Southeast Asia"
-VM_SIZE=Small
-CS_NAME=nerdswarm
-VM_IMAGE=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_10-amd64-server-20150202-en-us-30GB
+CS_NAME=nerdswarm-$NAME_SUFFIX
+SSH_KEY_FILE=output/swarm-ssh-$NAME_SUFFIX.key
+SSH_CERT=output/swarm-ssh-$NAME_SUFFIX.pem
+SSH_CONFIG_FILE=output/ssh-$NAME_SUFFIX.config
+VNET_NAME=swarmvnet-$NAME_SUFFIX
+
+# number of swarm worker nodes to delete
+SWARM_WORKER_NODES=3
 
 # remove ssh keys
-rm -f swarm-ssh.key 2> /dev/null
-rm -f swarm-ssh.pem 2> /dev/null
+rm -f $SSH_KEY_FILE 2> /dev/null
+rm -f $SSH_CERT 2> /dev/null
 
 # remove cached ssh keys
 ssh-keygen -R [$CS_NAME.cloudapp.net]:22000 -f ~/.ssh/known_hosts
@@ -18,16 +31,23 @@ ssh-keygen -R [$CS_NAME.cloudapp.net]:22002 -f ~/.ssh/known_hosts
 ssh-keygen -R [$CS_NAME.cloudapp.net]:22003 -f ~/.ssh/known_hosts
 
 # delete ssh config file
-rm ssh.config 2> /dev/null
+rm $SSH_CONFIG_FILE 2> /dev/null
 
 # delete vms
+echo azure vm delete -q swarm-master
 azure vm delete -q swarm-master
-azure vm delete -q swarm-00
-azure vm delete -q swarm-01
-azure vm delete -q swarm-02
+
+SWARM_NODES_TO=`expr $SWARM_WORKER_NODES - 1`
+for i in `seq 0 $SWARM_NODES_TO`;
+do
+	echo azure vm delete -q `printf "swarm-%02d" $i`
+	azure vm delete -q `printf "swarm-%02d" $i`
+done
 
 # delete cloud service
+echo azure service delete -q $CS_NAME
 azure service delete -q $CS_NAME
 
 # delete vnet
-azure network vnet delete -q swarmvnet
+echo azure network vnet delete -q $VNET_NAME
+azure network vnet delete -q $VNET_NAME
--- swarm-up.sh
+++ swarm-up.sh
@@ -7,76 +7,86 @@
 #		[3] Azure cross platform CLI - you can simply run:
 #         sudo npm install azure-cli -g
 
-# script parameters
-VNET_NAME=swarmvnet
-VNET_LOCATION="Southeast Asia"
+# randomly generated string used as a prefix for all Azure resource names
+NAME_SUFFIX=`node -e 'console.log(require("crypto").randomBytes(4).toString("hex"))'`
+
+# the name of the virtual network in which the VMs will be provisioned
+VNET_NAME=swarmvnet-$NAME_SUFFIX
+
+# Azure data center where the cluster will be deployed
+VNET_LOCATION="West US"
+
+# VM size to use
 VM_SIZE=Small
-CS_NAME=nerdswarm
-VM_IMAGE=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_10-amd64-server-20150202-en-us-30GB
+
+# name of the cloud service where the VMs will be hosted
+CS_NAME=dswarm-$NAME_SUFFIX
+
+# the Ubuntu Linux VM image to be used
+VM_IMAGE=b39f27a8b8c64d52b05eac6a62ebad85__Ubuntu-14_04_3-LTS-amd64-server-20150805-en-us-30GB
+
+# VM user name
 VM_USER_NAME=avranju
 
+# number of swarm worker nodes to create
+SWARM_WORKER_NODES=3
+
+# SSH keys for the VM
+SSH_KEY_FILE=output/swarm-ssh-$NAME_SUFFIX.key
+SSH_CERT=output/swarm-ssh-$NAME_SUFFIX.pem
+SSH_CONFIG_FILE=output/ssh-$NAME_SUFFIX.config
+
+# create the 'output' folder if it doesn't exist
+mkdir -p output
+
+# save the prefix into a file
+echo $NAME_SUFFIX > output/swarm-$NAME_SUFFIX.deployment
+
 # generate new ssl key
-openssl req -x509 -nodes -newkey rsa:2048 -subj '/O=Microsoft Open Technologies, Inc./L=Redmond/C=US/CN=msopentech.com' -keyout swarm-ssh.key -out swarm-ssh.pem
+openssl req -x509 -nodes -newkey rsa:2048 -subj '/O=Microsoft Open Technologies, Inc./L=Redmond/C=US/CN=msopentech.com' -keyout $SSH_KEY_FILE -out $SSH_CERT
 
 # set permissions on key file so ssh is happy
-chmod 400 swarm-ssh.key
-
-# remove old cached ssh key from ~/.ssh/known_hosts to avoid warning
-# when sshing later
-ssh-keygen -R [$CS_NAME.cloudapp.net]:22000 -f ~/.ssh/known_hosts
-ssh-keygen -R [$CS_NAME.cloudapp.net]:22001 -f ~/.ssh/known_hosts
-ssh-keygen -R [$CS_NAME.cloudapp.net]:22002 -f ~/.ssh/known_hosts
-ssh-keygen -R [$CS_NAME.cloudapp.net]:22003 -f ~/.ssh/known_hosts
+chmod 400 $SSH_KEY_FILE
 
 # create vnet
-azure network vnet create --location="$VNET_LOCATION" --address-space=172.16.0.0 $VNET_NAME
+echo Creating vnet $VNET_NAME
+azure network vnet create --location="$VNET_LOCATION" \
+	--address-space=172.16.0.0 $VNET_NAME
 
 # create master swarm node
-azure vm create -n swarm-master -e 22000 -z $VM_SIZE --virtual-network-name=$VNET_NAME $CS_NAME --ssh-cert=swarm-ssh.pem --no-ssh-password --custom-data ./cloud-init.sh $VM_IMAGE $VM_USER_NAME
+echo Creating docker swarm master node swarm-master
+azure vm create -n swarm-master -e 22000 -z $VM_SIZE \
+	--virtual-network-name=$VNET_NAME $CS_NAME \
+	--ssh-cert=$SSH_CERT --no-ssh-password \
+	--custom-data ./cloud-init.sh $VM_IMAGE $VM_USER_NAME
 
 # create worker swarm nodes
-azure vm create -n swarm-00 -e 22001 -z $VM_SIZE --virtual-network-name=$VNET_NAME $CS_NAME --ssh-cert=swarm-ssh.pem --no-ssh-password --custom-data ./cloud-init.sh --connect $VM_IMAGE $VM_USER_NAME
 
-azure vm create -n swarm-01 -e 22002 -z $VM_SIZE --virtual-network-name=$VNET_NAME $CS_NAME --ssh-cert=swarm-ssh.pem --no-ssh-password --custom-data ./cloud-init.sh --connect $VM_IMAGE $VM_USER_NAME
-
-azure vm create -n swarm-02 -e 22003 -z $VM_SIZE --virtual-network-name=$VNET_NAME $CS_NAME --ssh-cert=swarm-ssh.pem --no-ssh-password --custom-data ./cloud-init.sh --connect $VM_IMAGE $VM_USER_NAME
+SWARM_NODES_TO=`expr $SWARM_WORKER_NODES - 1`
+for i in `seq 0 $SWARM_NODES_TO`;
+do
+	echo Creating docker swarm worker node `printf "swarm-%02d" $i`
+	azure vm create -n `printf "swarm-%02d" $i` -e `expr 22001 + $i` -z $VM_SIZE \
+		--virtual-network-name=$VNET_NAME $CS_NAME \
+		--ssh-cert=$SSH_CERT --no-ssh-password \
+		--custom-data ./cloud-init.sh --connect $VM_IMAGE $VM_USER_NAME
+done
 
 # create ssh config file
-echo "Host swarm-master" > ssh.config
-echo "    User $VM_USER_NAME" >> ssh.config
-echo "    HostName $CS_NAME.cloudapp.net" >> ssh.config
-echo "    Port 22000" >> ssh.config
-echo "    IdentityFile ./swarm-ssh.key" >> ssh.config
-echo "    StrictHostKeyChecking no" >> ssh.config
+echo "Host swarm-master" > $SSH_CONFIG_FILE
+echo "    User $VM_USER_NAME" >> $SSH_CONFIG_FILE
+echo "    HostName $CS_NAME.cloudapp.net" >> $SSH_CONFIG_FILE
+echo "    Port 22000" >> $SSH_CONFIG_FILE
+echo "    IdentityFile ./$SSH_KEY_FILE" >> $SSH_CONFIG_FILE
+echo "    StrictHostKeyChecking no" >> $SSH_CONFIG_FILE
 
-echo "Host swarm-00" >> ssh.config
-echo "    User $VM_USER_NAME" >> ssh.config
-echo "    HostName $CS_NAME.cloudapp.net" >> ssh.config
-echo "    Port 22001" >> ssh.config
-echo "    IdentityFile ./swarm-ssh.key" >> ssh.config
-echo "    StrictHostKeyChecking no" >> ssh.config
-
-echo "Host swarm-01" >> ssh.config
-echo "    User $VM_USER_NAME" >> ssh.config
-echo "    HostName $CS_NAME.cloudapp.net" >> ssh.config
-echo "    Port 22002" >> ssh.config
-echo "    IdentityFile ./swarm-ssh.key" >> ssh.config
-echo "    StrictHostKeyChecking no" >> ssh.config
-
-echo "Host swarm-02" >> ssh.config
-echo "    User $VM_USER_NAME" >> ssh.config
-echo "    HostName $CS_NAME.cloudapp.net" >> ssh.config
-echo "    Port 22003" >> ssh.config
-echo "    IdentityFile ./swarm-ssh.key" >> ssh.config
-echo "    StrictHostKeyChecking no" >> ssh.config
-
-
-
-
-
-
-
-
-
-
-
+for i in `seq 0 $SWARM_NODES_TO`;
+do
+	HOST_PORT=`expr 22001 + $i`
+	echo `printf "Host swarm-%02d" $i` >> $SSH_CONFIG_FILE
+	echo "    User $VM_USER_NAME" >> $SSH_CONFIG_FILE
+	echo "    HostName $CS_NAME.cloudapp.net" >> $SSH_CONFIG_FILE
+	echo "    Port $HOST_PORT" >> $SSH_CONFIG_FILE
+	echo "    IdentityFile ./$SSH_KEY_FILE" >> $SSH_CONFIG_FILE
+	echo "    StrictHostKeyChecking no" >> $SSH_CONFIG_FILE	
+done
